cf79023ac6ac69707b836c79b7723853f4575be0
Lisa-Baumgaertner <46650897+Lisa-Baumgaertner@users.noreply.github.com>
2025-09-01 22:19:45 +0200

    Update batch_inference_adv_bench.py

--- inference/batch_inference_adv_bench.py
+++ inference/batch_inference_adv_bench.py
@@ -22,6 +22,10 @@ def hf_log_in():
 
 
 def batch_inference(model_path):
+    """Function to perform batch inference of the model.
+    
+    :param model_path: path to the model on huggingface.
+    """
     from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
     from peft import PeftModel, PeftConfig
     import pandas as pd
